ffb8568709994a2c955364a55576c2acbe0b8548
leticia <133894991+13martinlele@users.noreply.github.com>
2023-11-08 09:53:06 -0300

    Add files via upload

---
+++ Livros/src/main/java/com/gerenciamentodelivros/Livros/Service/LivroService.java
@@ -0,0 +1,53 @@
+package com.gerenciamentodelivros.Livros.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gerenciamentodelivros.Livros.Entities.Livro;
+import com.gerenciamentodelivros.Livros.Repositories.LivroRepository;
+
+
+
+@Service
+public class LivroService {
+
+	private final LivroRepository livroRepository;
+
+	@Autowired
+	public LivroService(LivroRepository livroRepository) {
+		this.livroRepository = livroRepository;
+	}
+
+	public Livro getLivroById(Long id) {
+		return livroRepository.findById(id).orElse(null);
+	}
+
+	public List<Livro> getAllLivro() {
+		return livroRepository.findAll();
+	}
+
+	public Livro saveLivro(Livro livro) {
+		return livroRepository.save(livro);
+	}
+
+	public void deleteLivro(Long id) {
+		livroRepository.deleteById(id);
+	}
+
+	public Livro updateLivro(Long id, Livro livro) {
+        Optional<Livro> livroOptional = livroRepository.findById(id);
+        if (livroOptional.isPresent()) {
+        	Livro livroExistente = livroOptional.get();
+        	livroExistente.setDescricao(livro.getDescricao());
+        	livroExistente.setIsbn(livro.getIsbn());          
+            return livroRepository.save(livroExistente); 
+        } else {
+            return null; 
+        }
+    
+	}
+
+}
